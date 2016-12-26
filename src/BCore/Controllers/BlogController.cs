44c6899efc786e3e3b8c7a2eb8aeecdf6d1fa709
gagarinbefree <gagarinbefree@gmail.com>
2016-12-26 18:23:46 +0500

    Suffered a logic from BlogController to Helper class

--- src/BCore/Controllers/BlogController.cs
+++ src/BCore/Controllers/BlogController.cs
@@ -8,6 +8,7 @@ using AutoMapper;
 using BCore.Dal.BlogModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
+using BCore.Models.Helpers;
 
 namespace BCore.Controllers
 {
@@ -28,13 +29,8 @@ namespace BCore.Controllers
         [ActionName("Index")]
         public async Task<IActionResult> IndexAsync()
         {
-            var posts = await _unit.PostRepository.GetAllAsync<DateTime>(
-                f => f.DateTime
-                , true
-                , f => f.UserId == _userManager.GetUserId(HttpContext.User) && f.Parts.Count > 0
-                , 50
-                , f => f.Parts);
-            
+            var posts = await BlogHelper.GetPostsByUser(_unit, _userManager, HttpContext.User);
+
             return View(Mapper.Map<WhatsNewViewModel>(posts));
         }
         
@@ -43,9 +39,7 @@ namespace BCore.Controllers
         {
             ModelState.Clear();
 
-            m.Parts.Add(Mapper.Map<PartViewModel>(m.Part));
-            m.Part.Text = String.Empty;
-            m.Part.ImageUrl = String.Empty;
+            BlogHelper.AddPartToPost(m);
 
             return PartialView("_Post", m);
         }
@@ -56,13 +50,7 @@ namespace BCore.Controllers
         {
             ModelState.Clear();
 
-            if (m.Parts.Count > 0)
-            {
-                var post = Mapper.Map<Post>(m);
-                post.UserId = _userManager.GetUserId(HttpContext.User);
-
-                await _unit.PostRepository.CreateAsync(post);
-            }
+            await BlogHelper.SubmitPostAsync(m, _unit, _userManager, HttpContext.User);
 
             TempData["messageStatus"] = new Random(DateTime.Now.Millisecond).Next(1, 1000);
 
@@ -72,7 +60,7 @@ namespace BCore.Controllers
         [ActionName("DeletePost")]
         public async Task<ActionResult> DeletePostAsync(Guid id)
         {
-            await _unit.PostRepository.DeleteAsync(new Dal.BlogModels.Post { Id = id });
+            await BlogHelper.DeletePostAsync(id, _unit);
 
             TempData["messageStatus"] = new Random(DateTime.Now.Millisecond).Next(1, 1000);
 
---
+++ src/BCore/Models/Helpers/PostHelper.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using BCore.Dal.BlogModels;
+using BCore.Dal.Ef;
+using BCore.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace BCore.Models.Helpers
+{
+    // to do DI
+    public static class BlogHelper
+    {
+        public static async Task<int> SubmitPostAsync(WhatsNewViewModel model, Unit _unit, UserManager<User> _userManager, ClaimsPrincipal user)
+        {
+            if (model.Parts.Count > 0)
+            {
+                var post = Mapper.Map<Post>(model);
+                post.UserId = _userManager.GetUserId(user);
+
+                return await _unit.PostRepository.CreateAsync(post);
+            }
+
+            return -1;
+        }
+
+        public static async Task<int> DeletePostAsync(Guid id, Unit _unit)
+        {
+            return await _unit.PostRepository.DeleteAsync(new Post { Id = id });
+        }
+
+        public static void AddPartToPost(WhatsNewViewModel model)
+        {
+            model.Parts.Add(Mapper.Map<PartViewModel>(model.Part));
+            model.Part.Text = String.Empty;
+            model.Part.ImageUrl = String.Empty;
+        }
+
+        public static async Task<ICollection<Post>> GetPostsByUser(Unit _unit, UserManager<User> _userManager, ClaimsPrincipal user)
+        {
+            return await _unit.PostRepository.GetAllAsync<DateTime>(
+                f => f.DateTime
+                , true
+                , f => f.UserId == _userManager.GetUserId(user) && f.Parts.Count > 0
+                , 50
+                , f => f.Parts);
+        }
+    }
+}
