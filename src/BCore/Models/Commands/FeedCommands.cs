2c9b4a725bf640001038a4f27d0cd5cd3130587e
gagarinbefree <gagarinbefree@gmail.com>
2017-03-11 23:51:32 +0500

    start work on feed view

---
+++ src/BCore/Models/Commands/FeedCommands.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using BCore.Dal;
+using BCore.Dal.Ef;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BCore.Models.Commands
+{
+    public class FeedCommands : IFeedCommands
+    {
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+        private IUoW _unit;
+
+        public FeedCommands(IUoW unit, IMapper map, UserManager<User> userManager)
+        {
+            _userManager = userManager;
+            _mapper = map;
+            _unit = unit;
+        }
+    }
+}
