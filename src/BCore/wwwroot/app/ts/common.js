befb52d5612e42d12e6afa845cb9788a309d072e
gagarinbefree <gagarinbefree@gmail.com>
2017-04-02 22:02:04 +0500

    work at top raits label

--- src/BCore/wwwroot/app/ts/common.js
+++ src/BCore/wwwroot/app/ts/common.js
@@ -1,23 +1,28 @@
+/// <reference path="../typings/jquery/jquery.d.ts" />
 /// <reference path="../typings/bootbox/index.d.ts" />
 var Common;
 (function (Common) {
     var App = (function () {
         function App() {
-            this.confirm = new Confirm();
+            this.confirm = new ConfirmDialog();
         }
         return App;
     }());
     Common.App = App;
-    var Confirm = (function () {
-        function Confirm() {
+    var ConfirmDialog = (function () {
+        function ConfirmDialog() {
             var _this = this;
             this.elConfirm = $('a[data-confirm]');
-            this.elConfirm.on('click', function (e) { return _this.confirm(e); });
+            this.elConfirm.on('click', function (e) { return _this.showDialog(e); });
         }
-        Confirm.prototype.confirm = function (e) {
+        ConfirmDialog.prototype.showDialog = function (e) {
+            var _this = this;
             e.preventDefault();
+            this.elTarget = $(e.target);
             bootbox.confirm({
-                message: $(e.target).attr('data-confirm'),
+                title: this.elTarget.attr('data-confirm-title'),
+                message: this.elTarget.attr('data-confirm'),
+                size: 'small',
                 buttons: {
                     confirm: {
                         label: 'Yes',
@@ -28,14 +33,18 @@ var Common;
                         className: 'btn-success'
                     }
                 },
-                callback: function (result) {
-                    if (result) {
-                    }
-                }
+                callback: function (result) { return _this.handlerDialogResult(result); }
             });
         };
-        return Confirm;
+        ConfirmDialog.prototype.handlerDialogResult = function (result) {
+            if (result) {
+                var href = this.elTarget.attr('href');
+                if (typeof (href) !== "undefined" && href && href.length > 0)
+                    window.location.href = href;
+            }
+        };
+        return ConfirmDialog;
     }());
-    Common.Confirm = Confirm;
+    Common.ConfirmDialog = ConfirmDialog;
 })(Common || (Common = {}));
 //# sourceMappingURL=common.js.map
