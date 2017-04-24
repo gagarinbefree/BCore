7381fc1fa95b5a707a8fe050524bf0d83758b8b8
gagarinbefree <gagarinbefree@gmail.com>
2017-04-24 17:54:59 +0500

    add highlight.js to bower config file. add starts class to DAL

---
+++ src/BCore/Dal/BlogModels/Stars.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BCore.Dal.BlogModels
+{
+    public class Stars
+    {
+        public string UserId { set; get; }
+        public DateTime DateTime { set; get; }        
+        [ForeignKey("Post")]
+        public Guid PostId { set; get; }
+        public virtual Post Post { set; get; }
+    }
+}
